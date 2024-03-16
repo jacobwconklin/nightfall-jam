eec4447014f97515afcf01cbb53905b158cbe70a
Mitch <97319247+SpruceChipper@users.noreply.github.com>
2024-03-16 13:32:56 -0400

    Update ChargeDisplay Script
    Has functionality for spentCharge, not for addCharge yet, but isDay() will help

--- Assets/Scripts/HealthDisplay.cs
+++ Assets/Scripts/HealthDisplay.cs
@@ -14,11 +14,45 @@ public class HealthDisplay : MonoBehaviour
     }
 
     //Total amt of health
-    float charge = 5f;
+    float charge = 100f;
     public TextMeshProUGUI chargeText;
     // Update is called once per frame
     void Update()
     {
-        
+        chargeText.text = "Charge: " + charge;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spentCharge(5);
+        }
+
+        /*if (isDay())
+        {
+            addCharge(1);
+        }*/
+    }
+
+    public void spentCharge(float amt)
+    {
+        if (charge - amt <= 0)
+        {
+            charge = 0;
+        }
+        else
+        {
+            charge -= amt;
+        }
+    }
+
+    public void addCharge(float amt)
+    {
+        if (charge + amt >= 100)
+        {
+            charge = 100;
+        }
+        else
+        {
+            charge += amt;
+        }
     }
 }
