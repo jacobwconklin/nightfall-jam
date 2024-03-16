c203d488644249f2d4051616a5f6bc23eaf8c1da
Jacob Conklin <jacobyconklin@gmail.com>
2024-03-16 14:36:51 -0400

    PLAYER STATUS LETS GO

--- Assets/Scripts/PlayerAndEnemies/PlayerStatus.cs
+++ Assets/Scripts/PlayerAndEnemies/PlayerStatus.cs
@@ -8,13 +8,14 @@ public class PlayerStatus : PlayerEnemy
     [SerializeField] private HealthDisplay healthDisplay;
     [SerializeField] private float chargePerSecond = 1;
     [SerializeField] private float maxCharge = 100;
-    private float charge = maxCharge;
+    private float charge;
     private EventManager eventManager;
     private bool inShadow = false;
 
     private void Start()
     {
         eventManager = EventManager.EventManagerInstance;
+        charge = 50;
     }
 
     // Inherits from PlayerEnemy abstract class, handles tracking player's charge and health.
@@ -22,19 +23,19 @@ public class PlayerStatus : PlayerEnemy
     public override void DealDamage(float Damage)
     {
         Health -= Damage;
-        // healthDisplay.setHealth(Health);
+        healthDisplay.spenthealth(Health);
     }
 
     public override void SetHealth(float hp)
     {
         Health = hp;
-        // healthDisplay.setHealth(Health);
+        healthDisplay.spenthealth(Health);
     }
 
     private void spendCharge(float amount)
     {
         charge = charge - amount < 0 ? 0 : charge - amount;
-        // chargeDisplay.setCharge(charge);
+        chargeDisplay.setCharge(charge);
     }
 
     // Gain charge overtime
@@ -44,7 +45,23 @@ public class PlayerStatus : PlayerEnemy
         {
             // Regain charge
             charge = charge + 1 * Time.deltaTime > maxCharge ? maxCharge : charge + 1 * Time.deltaTime;
-            // chargeDisplay.setCharge(charge);
+            chargeDisplay.setCharge(charge);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Shadow"))
+        {
+            inShadow = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Shadow"))
+        {
+            inShadow = false;
         }
     }
 
