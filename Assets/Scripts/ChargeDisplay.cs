1f16250df467f2512668decc6309e5f7d1a5c998
Mitch <97319247+SpruceChipper@users.noreply.github.com>
2024-03-16 12:18:39 -0400

    Update 3/16 12:18pm
    Unknown

---
+++ Assets/Scripts/ChargeDisplay.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ChargeDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    //Total amt of solar charge
+     float charge = 100f;
+    public TextMeshProUGUI chargeText;
+    // Update is called once per frame
+    void Update()
+    {
+        chargeText.text = "Charge : " + charge;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            charge--;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            charge++;
+        }
+    }
+ 
+    public void spentCharge(float amount)
+    {
+        if (charge - amount <= 0)
+        {
+            charge = 0;
+        }
+        else
+        {
+            charge -= amount;
+        }
+    }
+
+    public void addCharge(float amount)
+    {
+        if (charge + amount > 100)
+        {
+            charge = 100;
+        }
+        else
+        {
+            charge += amount;
+        }
+    }
+}
