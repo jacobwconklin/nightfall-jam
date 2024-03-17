606d00a3c680872f56090e44139a3b1341b36bbb
DerekBob <130794291+DerekBob@users.noreply.github.com>
2024-03-16 23:03:26 -0400

    Zombies now hit and shoot at the player

--- Assets/Scripts/PlayerAndEnemies/EnemyAI.cs
+++ Assets/Scripts/PlayerAndEnemies/EnemyAI.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System;
 using UnityEngine.AI;
 using UnityEngine;
 
 public interface IEnemySetup
 {
-    void Setup(float Damage, GameObject Gun, float Health);
+    void Setup(float Damage, int Gun, float Health);
 }
 public class EnemyAI : PlayerEnemy, IEnemySetup
 {
@@ -13,25 +14,91 @@ public class EnemyAI : PlayerEnemy, IEnemySetup
     public GameObject Player;
     public EnemyState State = EnemyState.Seeking;
     public float Damage = 10; //the event will set this
+    public float GunDamage = 10; //the event will set this
 
     //For shooting Enemies
     public bool HasGun = false;
-    public GameObject Gun = null;
+    public int Gun = 0; //Gun will be the index of the array
+    public int Ammo;
+    public float GDPS = 2;
+    private float RGDPS = 2;
+
+    private Animator anim;
+
+    public float DPS = 2; //Damage Per Second
+    private float RDPS; //Anything with R means to remember
+
+    public float MeleeRange = 4;
+    public float ShootingRange = 11;
+
+    public float Speed = 3.5f;
+
+    private bool StopAll = false; //to stop the death spam
+
+    [Header("Drag Guns Here")]
+    [SerializeField] private GameObject[] Guns; //NEEDS TO BE RIFLE SMG PISTOL
+    //[SerializeField] private GameObject rifle;
+    //[SerializeField] private GameObject smg;
+    //[SerializeField] private GameObject pistol;
+
+    [SerializeField] private GameObject BulletAttach;
+    
+    
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        RDPS = DPS;
+        agent.speed = Speed;
+
         SetHealth(100);
         if(Player == null)
         {
             State = EnemyState.Idle;
             Debug.LogError("There is no Player to track!");
+            
         }
 
-        if(Gun != null)
+        Guns[0].SetActive(false);
+        Guns[1].SetActive(false);
+        Guns[2].SetActive(false);
+        if (Gun != 0)
         {
+            Gun--; //because the setup returned a value a little higher. 
             HasGun = true;
+            Guns[Gun].SetActive(true);
+
+            //This is why I wanted the gun script to be used by both player and npc, so I dont have to do this ififfifif.
+            if (Gun == 0)
+            {
+                Ammo = 32;
+                GunDamage = 10;
+                GDPS = 0.7f;
+                RGDPS = 0.7f;
+            }
+
+            if (Gun == 1)
+            {;
+                Ammo = 60;
+                GunDamage = 5;
+                GDPS = 0.2f;
+                RGDPS = 0.2f;
+            }
+
+            if (Gun == 2)
+            {
+                Ammo = 16;
+                GunDamage = 2;
+                GDPS = 1;
+                RGDPS = 1f;
+            }
         }
+
+        anim.SetTrigger("Moving");
+
+        
+
     }
 
     private void Update()
@@ -44,36 +111,100 @@ public class EnemyAI : PlayerEnemy, IEnemySetup
         switch (State)
         {
             case EnemyState.Seeking:
+
                 agent.destination = Player.transform.position;
 
-                if (Vector3.Distance(this.transform.position, Player.transform.position) < 2)
+                if(Vector2DDist(this.transform.position, Player.transform.position) <= ShootingRange && HasGun && CheckForObstruction())
                 {
-                    Player.GetComponent<IDamage>().DealDamage(Damage);
+                    State = EnemyState.Shoot;
+                    agent.isStopped = true;
+                    break;
+                }
+
+                if (Vector2DDist(this.transform.position, Player.transform.position) <= MeleeRange)
+                {
                     State = EnemyState.Fight;
+                    agent.isStopped = true;
                 }
                 break;
             case EnemyState.Fight:
-
+                
                 //Play the fight animation Here
-
-                if (Vector3.Distance(this.transform.position, Player.transform.position) <= 1)
+                if(DPS == RDPS)
                 {
-                    Player.GetComponent<IDamage>().DealDamage(Damage);
-                   
-                }
-                else
-                {
-                    State = EnemyState.Seeking;
+                    anim.SetTrigger("Attack");
+
+                    if (Vector2DDist(this.transform.position, Player.transform.position) <= MeleeRange && Player.GetComponent<IDamage>() != null)
+                    {
+                        Player.GetComponent<IDamage>().DealDamage(Damage);
+                    }
+                    else if (Vector2DDist(this.transform.position, Player.transform.position) > MeleeRange)
+                    {
+                        State = EnemyState.Seeking;
+                        agent.isStopped = false;
+                        DPS = RDPS;
+                        anim.ResetTrigger("Attack");
+                        anim.SetTrigger("Moving");
+                        
+                        break;
+                    }
                 }
 
+                DamagePerSec();
+              
                 break;
             case EnemyState.Dying:
-                //play dying Animation Here
-                //couroutine the destroy game object here as well
-                Destroy(gameObject);
+
+                if(!StopAll)
+                {
+                    anim.ResetTrigger("Attack");
+                    anim.ResetTrigger("Moving");
+                    anim.SetTrigger("OverHeat");
+                    agent.isStopped = true;
+                    StopAll = true;
+                    StartCoroutine(TrueDeath(5));
+                }
+                
+                //if it had the gun then make it drop it here
+                break;
+            case EnemyState.Shoot:
+
+                transform.rotation = Quaternion.LookRotation((Player.transform.position - transform.position).normalized);
+
+                if (GDPS == RGDPS && Ammo != 0)
+                {
+
+                    if (Vector2DDist(this.transform.position, Player.transform.position) <= ShootingRange && CheckForObstruction())
+                    {
+                        Shoot();
+                    }
+                    else if (Vector2DDist(this.transform.position, Player.transform.position) > ShootingRange || !CheckForObstruction())
+                    {
+                        State = EnemyState.Seeking;
+                        agent.isStopped = false;
+                        GDPS = RGDPS;
+
+                        anim.SetTrigger("Moving");
+                        break;
+                    }
+                }
+
+                if(Ammo == 0)
+                {
+                    HasGun = false;
+                    State = EnemyState.Seeking;
+                    agent.isStopped = false;
+                    Guns[Gun].SetActive(false);
+                    //AND MAKE IT SPAWN THE GUN HERE
+                    break;
+                }
+
+                GDamagePerSec();
+
                 break;
             case EnemyState.Idle:
-                //play Idle animation here
+                anim.SetTrigger("Idle");
+                agent.isStopped = true;
                 break;
         }
 
@@ -81,11 +212,77 @@ public class EnemyAI : PlayerEnemy, IEnemySetup
         
     }
 
-    public void Setup(float Dmg, GameObject Gun, float Health)
+    //will return true if player is unobstructed
+    private bool CheckForObstruction()
+    {
+        RaycastHit hit;
+        Vector3 line = Player.transform.position - transform.position;
+        Physics.Raycast(this.transform.position, Player.transform.position - transform.position, out hit, line.magnitude);
+
+        
+        if(Physics.Raycast(this.transform.position, Player.transform.position - transform.position, out hit, line.magnitude))
+        {
+            if(hit.collider.gameObject == Player.gameObject)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void Shoot()
+    {
+        GameObject Bullet = Instantiate(Resources.Load("BulletHitBox", typeof(GameObject)), BulletAttach.transform.position, BulletAttach.transform.rotation) as GameObject;
+        Bullet.GetComponent<IBullet>().SetDamage(Damage);
+
+        Ammo--;
+
+        Guns[Gun].GetComponent<Animator>().SetTrigger("Shooting");
+    }
+    //unused
+    private IEnumerator ChangeState(EnemyState state, float time)
+    {
+        yield return new WaitForSeconds(time);
+        State = state;
+    }
+
+    private IEnumerator TrueDeath(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+
+    public void Setup(float Dmg, int Gun, float Health)
     {
         Damage = Dmg;
         this.Gun = Gun;
         MaxHealth = Health;
+    }
+
+    private float Vector2DDist(Vector3 Me, Vector3 player)
+    {
+        float x = Me.x - player.x;
+        float z = Me.z - player.z;
+        return Mathf.Sqrt((x * x) + (z * z));
+    }
+
+    private void DamagePerSec()
+    {
+        DPS -= Time.deltaTime;
+        if (DPS <= 0)
+        {
+            DPS = RDPS;
+        }
+    }
+
+    private void GDamagePerSec()
+    {
+        GDPS -= Time.deltaTime;
+        if (GDPS <= 0)
+        {
+            GDPS = RGDPS;
+        }
     }
 
     public enum EnemyState
@@ -95,7 +292,6 @@ public class EnemyAI : PlayerEnemy, IEnemySetup
         Shoot,
         Dying,
         Idle,
-        OverHeating,
-        Flee
+        Flee //got too complicated
     }
 }
--- Assets/Scripts/SpawnNodes/EnemySpawn.cs
+++ Assets/Scripts/SpawnNodes/EnemySpawn.cs
@@ -10,31 +10,52 @@ public class EnemySpawn : MonoBehaviour, IEnemySetup
     public int NumEnemy = 10;
 
     public float Damage = 10;
-    public GameObject Gun = null;
     public float MaxHealth = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         //Put yourself into the event manager
-        SpawnEnemies(10);
+        //SpawnEnemies(10);
     }
 
     public void SpawnEnemies(int Num)
     {
-        for(int i = 0; i <= Num; i++)
+        for(int i = 0; i < Num; i++)
         {
-            Vector3 RandomPos = transform.position + new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
-            Enemy.GetComponent<IEnemySetup>().Setup(Damage, null, MaxHealth);
+            Vector3 RandomPos = transform.position + new Vector3(Random.Range(0, this.transform.localScale.x), 0, Random.Range(0, this.transform.localScale.z));
+            Enemy.GetComponent<IEnemySetup>().Setup(Damage, PickGun(), MaxHealth); // call a chance table right here
             Instantiate(Enemy, RandomPos, Quaternion.identity);
         }
     }
 
-    public void Setup(float Dmg, GameObject Gun, float Health)
+    //Always make gun null!
+    public void Setup(float Dmg, int Gun, float Health)
     {
         Damage = Dmg;
-        this.Gun = Gun;
         MaxHealth = Health;
+    }
+
+    public int PickGun()
+    {
+        float RandomValue = Random.value;
+
+        float[] probabilities = { 0.7f, 0.5f, 0.10f, 0.15f };
+        //string[] Weapon = { null, "Rifle", "SMG", "Pistol" };
+
+        //Debug.Log(RandomValue);
+        float cumulativeProbability = 0;
+        for (int i = 0; i < probabilities.Length; i++)
+        {
+            cumulativeProbability += probabilities[i];
+            if (RandomValue <= cumulativeProbability)
+            {
+                return i;
+            }
+        }
+
+        //This should never return
+        return 0;
     }
 
 }
